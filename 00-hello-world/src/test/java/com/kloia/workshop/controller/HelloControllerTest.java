5741e96d5f48c5cc1dac18747fb240d9527f870f
Burak Bozan <burakbozan@Buraks-MacBook-Pro.local>
2018-11-10 15:56:10 +0300

    first commit

---
+++ 00-hello-world/src/test/java/com/kloia/workshop/controller/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.kloia.workshop.controller;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+
+    @Test
+    public void hello() {
+        System.out.println("Unit test is running...");
+    }
+
+}

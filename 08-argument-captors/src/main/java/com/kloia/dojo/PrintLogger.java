0fd38921a5ab564ea5ccdf42c7f110f17cb8000d
GitHub brkbzn <burakbozan@gmail.com>
2018-11-13 20:57:33 +0300

    fix

--- 08-argument-captors/src/main/java/com/kloia/dojo/PrintLogger.java
+++ 08-argument-captors/src/main/java/com/kloia/dojo/PrintLogger.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 public class PrintLogger {
 
     public void log(String message, boolean debugEnabled) {
-        System.out.println(message);
+        if (debugEnabled) {
+            System.out.println(message);
+        }
     }
 
 }
